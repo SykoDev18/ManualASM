cb0799b18015788b66157ca0dcd7e91e7058159e
Mark Miranda <mi410577@uaeh.edu.mx>
2025-03-27 06:08:16 -0600

    Revert "deploy"
    This reverts commit c818f6be90c65493c06f1991420c0ea5e590f9dc.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop/>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
